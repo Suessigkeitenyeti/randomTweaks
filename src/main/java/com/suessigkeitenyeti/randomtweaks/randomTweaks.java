32b6b0c5bdc60acbf965439417025f1af688ab29
Suessigkeitenyeti <suessigkeitenyeti@googlemail.com>
2015-05-26 00:49:02 +0200

    ep1-3

---
+++ src/main/java/com/suessigkeitenyeti/randomtweaks/randomTweaks.java
@@ -0,0 +1,30 @@
+package com.suessigkeitenyeti.randomtweaks;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Marius on 26.05.2015.
+ */
+@Mod(modid = "randomTweaks", version = "1.7.10-1.0")
+public class randomTweaks {
+    @Mod.Instance("randomTweaks")
+    public static randomTweaks instance;
+
+    @Mod.EventHandler
+    public void preInt(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
